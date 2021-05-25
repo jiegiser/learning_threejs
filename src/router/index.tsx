a6eb0c80d7ef7f4f60fa38e51a7e721126ea4666
jiegiser <cjieouba@outlook.com>
2021-05-25 10:20:24 +0800

    Feat: 新增流动线

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import {HashRouter, Route, Switch} from 'react-router-dom'
 import BasicSkeleton from '../pages/numOneCap/01-basic-skeleton'
 import FirstScene from '../pages/numOneCap/02-first-scene'
+import MeshLine from '../pages/MeshLine'
 
 
 const BasicRoute = () => (
@@ -9,6 +10,7 @@ const BasicRoute = () => (
     <Switch>
       <Route exact path="/" component={BasicSkeleton}/>
       <Route exact path="/first-scene" component={FirstScene}/>
+      <Route exact path="/MeshLine" component={MeshLine} />
     </Switch>
   </HashRouter>
 )
